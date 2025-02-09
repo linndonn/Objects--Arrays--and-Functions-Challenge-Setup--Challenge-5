c4a768bc4fa8645d19e289d306ac2f0b21882ad5
linndonn <linndonn@usf.edu>
2025-02-09 03:10:18 -0500

    Task 2 - Order Details

--- cc_5.js
+++ cc_5.js
@@ -6,3 +6,16 @@ let customer = { name: "John Doe", age: 35, email: "john.doe@email.com" };
 console.log(`Cusomter's Name: ${customer.name}`);
 console.log(`Customer's Age: ${customer.age}`);
 console.log(`Customer's Email: ${customer.email}`);
+
+//Task 2: Object Methods
+
+//Declaring an object order with the properties:
+let order = { orderId: 12345, totalAmount: 150, status: "Processing",
+//Displaying order deatils using a  displayOrder function
+displayOrder() {
+    console.log(`Order ID: ${this.orderId}`);
+    console.log(`Order Total Amount: ${this.totalAmount}`);
+    console.log(`Order Status: ${this.status}`);
+}
+};
+order.displayOrder();
