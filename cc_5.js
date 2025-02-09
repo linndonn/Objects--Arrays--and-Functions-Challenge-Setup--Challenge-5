6bf962bcabf5cbef9c6b6b369f949ee9cb78a9eb
linndonn <linndonn@usf.edu>
2025-02-09 02:56:03 -0500

    Task 1 - Customer Profile

---
+++ cc_5.js
@@ -0,0 +1,8 @@
+//Task 1: Object Properties
+
+//Declaring an object customer with the properties:
+let customer = { name: "John Doe", age: 35, email: "john.doe@email.com" };
+//Logging each obect into the console.
+console.log(`Cusomter's Name: ${customer.name}`);
+console.log(`Customer's Age: ${customer.age}`);
+console.log(`Customer's Email: ${customer.email}`);
